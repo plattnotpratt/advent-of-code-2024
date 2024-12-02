45a07c290a38cbba59e17687f119b5619b474988
w3cj <cj@null.computer>
2024-12-01 20:24:08 -0700

    Initial + 1st day of AOC

---
+++ 2024/day_1/q1.js
@@ -0,0 +1,53 @@
+const fs = require('node:fs/promises');
+async function getData(file) {
+  try {
+    const data = await fs.readFile(file , { encoding: 'utf8' });
+    return formatData(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function formatData(data){
+    const lines = data.split('\n');
+    const listA = [];
+    const listB = [];
+
+    for( let i = 0; i < lines.length; i++){
+        const line = lines[i].split('   ');
+        if(line[0] === ''){
+            continue;
+        }
+        if(line[1] === ''){
+            continue;
+        }
+        listA.push(line[0]);
+        listB.push(line[1]);
+    }
+    const fData = {
+        listA: listA,
+        listB: listB,
+    }
+    //console.log(fData);
+    return fData;
+}
+
+async function  main(){
+    let data = await getData("data.txt");
+    const listA = data.listA.sort();
+    const listB = data.listB.sort();
+    console.log(listA);
+    let answer = 0;
+    for( let i = 0; i < listB.length; i++){
+
+        answer += (Math.max(listA[i],listB[i]) - Math.min(listA[i],listB[i]))
+    }
+
+    console.log(answer);
+
+
+    
+}
+
+main();
+
---
+++ 2024/day_1/q2.js
@@ -0,0 +1,63 @@
+const fs = require('node:fs/promises');
+async function getData(file) {
+  try {
+    const data = await fs.readFile(file , { encoding: 'utf8' });
+    return formatData(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function formatData(data){
+    const lines = data.split('\n');
+    const listA = [];
+    const listB = [];
+
+    for( let i = 0; i < lines.length; i++){
+        const line = lines[i].split('   ');
+        if(line[0] === ''){
+            continue;
+        }
+        if(line[1] === ''){
+            continue;
+        }
+        listA.push(line[0]);
+        listB.push(line[1]);
+    }
+    const fData = {
+        listA: listA,
+        listB: listB,
+    }
+    //console.log(fData);
+    return fData;
+}
+
+function similarityScore(num, list){
+    mult = 0;
+    for(let i = 0; i < list.length; i++){
+        if(list[i] == num){
+            mult++;
+        }
+    }
+    
+    return num * mult;
+}
+async function  main(){
+    let data = await getData("data.txt");
+    const listA = data.listA.sort();
+    const listB = data.listB.sort();
+    console.log(listA);
+    let answer = 0;
+    for( let i = 0; i < listB.length; i++){
+
+        answer += similarityScore(listA[i], listB)
+    }
+
+    console.log(answer);
+
+
+    
+}
+
+main();
+
